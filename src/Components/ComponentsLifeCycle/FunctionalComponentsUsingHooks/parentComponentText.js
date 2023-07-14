fd545b3db0cef59e1aa4ebe41b857e85fa68f321
singamreddyanitha <singamreddyanitha123@gmail.com>
2023-07-14 18:24:38 +0530

    first Commit

---
+++ src/Components/ComponentsLifeCycle/FunctionalComponentsUsingHooks/parentComponentText.js
@@ -0,0 +1,12 @@
+import MainPage from  "./Components/FunctionalComponentsUsingHooks/MainPage"
+
+function App () {
+    return (
+        <>
+        <MainPage />
+        </>
+
+    )
+}
+
+export default App
